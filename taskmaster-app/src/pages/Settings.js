c72024c425a46c214b9f389646e075060ba50788
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-04-12 23:22:18 +0100

    added settings function page just to change email password and account settings display

---
+++ taskmaster-app/src/pages/Settings.js
@@ -0,0 +1,14 @@
+function Settings() {
+    return (
+      <div className="settings-page">
+        <h2>Settings</h2>
+  
+        <section>
+          <h3>👤 Account Settings</h3>
+          <p>Change email / password</p>
+        </section>
+
+      </div>
+    );
+  }
+  
