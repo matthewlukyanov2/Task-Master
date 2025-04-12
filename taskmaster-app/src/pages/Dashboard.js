0dc3ca53e4a85734ae3d968e3796af71f55dcc8e
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-04-12 23:20:49 +0100

    added dashboard content to dashboard class to display tasks

---
+++ taskmaster-app/src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+function Dashboard() {
+    return (
+      <div className="dashboard">
+        <h2>Today's Tasks</h2>
+        {/* Later: map tasks here */}
+        <ul>
+          <li><input type="checkbox" /> Finish UI prototype</li>
+          <li><input type="checkbox" /> Record voiceover</li>
+          <li><input type="checkbox" /> Upload PDF to Moodle</li>
+        </ul>
+      </div>
+    );
+  }
