43472199917c5595b94d39a0e99ea838b4f4314c
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-04-15 15:23:42 +0100

    added all necessary imports to app.js

--- taskmaster-app/src/App.js
+++ taskmaster-app/src/App.js
@@ -2,10 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import Settings from './pages/Settings';
+import AccountSettings from './pages/AccountSettings';
+import NotificationPreferences from './pages/NotificationPreferences';
+import Appearance from './pages/Appearance';
 
 function App() {
   return (
