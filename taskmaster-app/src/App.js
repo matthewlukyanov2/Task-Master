9bb1867675ba55c220bbb56341da75ad093ed61b
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-04-16 15:24:24 +0100

    added route to home page so it opens up to homepage on launch

--- taskmaster-app/src/App.js
+++ taskmaster-app/src/App.js
@@ -15,7 +15,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/settings" element={<Settings />} />
